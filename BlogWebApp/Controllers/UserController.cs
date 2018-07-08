2df5959be254b8bf1e4a96acfd3f35b9a652c14f
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-07-08 13:33:34 +0300

    Added basic UserDetails pages

--- BlogWebApp/Controllers/UserController.cs
+++ BlogWebApp/Controllers/UserController.cs
@@ -11,18 +11,18 @@ namespace BlogWebApp.Controllers
 {
     public class UserController : Controller
     {
-        private readonly BlogLoadService _blogLoadService;
-
-        public UserController(BlogLoadService blogLoadService)
-        {
-            _blogLoadService = blogLoadService;
-        }
-
         public IActionResult Index()
         {
             List<User> users = BlogData.Users;
 
             return View(users);
+        }
+
+        public IActionResult UserDetails(int userId)
+        {
+            User user = BlogData.Users.FirstOrDefault(u => u.Id.Equals(userId));
+
+            return View(user);
         }
     }
 }
