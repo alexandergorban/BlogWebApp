1d744159b34cc6bc6943dceb34dbe833d8b6b5fc
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-07-08 20:45:51 +0300

    Added UserQueryViewModel

---
+++ BlogWebApp/ViewModels/UserQueryViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlogWebApp.Models;
+
+namespace BlogWebApp.ViewModels
+{
+    public class UserQueryViewModel
+    {
+        public User User { get; set; }
+        public Post LastPost { get; set; }
+        public int LastPostCommentsCount { get; set; }
+        public int UncompletedTodosCount { get; set; }
+        public Post PostWithMaxComments { get; set; }
+        public Post PostWithMaxLikes { get; set; }
+    }
+}
