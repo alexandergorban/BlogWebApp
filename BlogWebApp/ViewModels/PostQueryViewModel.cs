39c0a5253d53111f356f475cabd08be65c87d1fd
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-07-08 20:47:24 +0300

    Added PostQueryViewModel

---
+++ BlogWebApp/ViewModels/PostQueryViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlogWebApp.Models;
+
+namespace BlogWebApp.ViewModels
+{
+    public class PostQueryViewModel
+    {
+        public Post Post { get; set; }
+        public Comment LongestComment { get; set; }
+        public Comment MostLikedComment { get; set; }
+        public int MostUnpopularCommentsCount { get; set; }
+    }
+}
