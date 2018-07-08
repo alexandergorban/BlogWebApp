39152948a49ed0c7d4e96c521f78f022875a6d27
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-07-08 22:03:00 +0300

    Added basic general pages

---
+++ BlogWebApp/Controllers/GeneralController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlogWebApp.Models;
+using BlogWebApp.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogWebApp.Controllers
+{
+    public class GeneralController : Controller
+    {
+        private readonly BlogQueriesService _blogQueriesService;
+
+        public GeneralController(BlogQueriesService blogQueriesService)
+        {
+            _blogQueriesService = blogQueriesService;
+        }
+
+        public IActionResult Index()
+        {
+            List<User> users = _blogQueriesService.GetComplexlyFilledUsers();
+
+            return View(users);
+        }
+    }
+}
